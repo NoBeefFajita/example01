09775f6e00118c2745f33a797fcc32ad93db40a9
NoBeefFajita <97187684+NoBeefFajita@users.noreply.github.com>
2022-03-29 03:04:18 +0900

    Update Exam06.java

--- f220328/Exam06/Exam06.java
+++ f220328/Exam06/Exam06.java
@@ -2,11 +2,16 @@ package example01.f220328.Exam06;
 
 public class Exam06 {
     public static void main(String[] args) {
+        boolean primeNumber;
+
         for (int num = 10; num < 100; num++) {
+            primeNumber = true;
             for (int i = 2; i < num / 2; i++) {
                 if (num % i == 0)
-                    continue;
+                    primeNumber = false;
             }
+            if (primeNumber)
+                System.out.println(num);
         }
     }
 }
