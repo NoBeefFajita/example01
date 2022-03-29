e7dd5ccf0b379c3bd626c48bab43cae8ae3bf3af
NoBeefFajita <97187684+NoBeefFajita@users.noreply.github.com>
2022-03-30 02:04:04 +0900

    Update Exam05.java

--- g220329/Exam05.java
+++ g220329/Exam05.java
@@ -2,14 +2,16 @@ package youngJun.java.day15.exam;
 
 public class Exam05 {
     public static void main(String[] args) {
-        int n = 5;
+        tillSum(5);
+    }
+
+    public static void tillSum(int n) {
+        
         int sum = 0;
-
-        System.out.println("1부터 n번까지의 합을 구합니다. n의 값: " + n);
-
         for (int i = 1; i <= n; i++)
             sum += i;
 
+        System.out.println("1부터 n번까지의 합을 구합니다. n의 값: " + n);
         System.out.println("1부터 n까지의 합은 " + sum + "입니다.");
     }
 }
