9adaf5c698f53d3a7797a89578fd94632ecb3603
NoBeefFajita <tjdudwns96@gmail.com>
2022-03-31 01:49:20 +0900

    추가

---
+++ b220318/ExerciseArray03.java
@@ -0,0 +1,32 @@
+package example01.b220318;
+
+import java.util.Scanner;
+
+public class ExerciseArray03 {
+
+    public static void main(String...args) {
+
+        Scanner sc = new Scanner(System.in);
+
+            System.out.print("첫번째 숫자 입력: ");        // 1~9 사이 숫자 입력
+            int num1 = sc.nextInt();
+            System.out.print("두번째 숫자 입력: ");
+            int num2 = sc.nextInt();
+
+            if (num1 > 9 || num1 < 1 || num2 > 9 || num2 < 1){
+                System.out.println("1~9 사이의 숫자를 입력하시오");
+                return;
+            }
+
+        int[][] gugudan = new int[10][10];          // 구구단 저장할 2차원 배열 생성 및 초기화
+
+        for(int i = 0; i < 10; i++){
+            if(i==0) continue;
+            for(int j = 0; j < 10; j++){
+                gugudan[i][j] = i * j;
+            }
+        }
+
+        System.out.println(num1 + " * " + num2 + " = " + gugudan[num1][num2]);
+    }
+}
---
+++ b220318/ExerciseClass10.java
@@ -0,0 +1,74 @@
+package example01.b220318;
+
+import java.util.Scanner;
+
+public class ExerciseClass10 {
+
+    public static void main(String[] args) {
+
+        CoinCase cc = new CoinCase();
+        Scanner sc = new Scanner(System.in);
+
+        int[] coin = cc.coin;
+
+        for (int i = 0; i < 10; i++) {          // addCoins 메소드로 동전 추가 10회
+            System.out.println((i + 1) + "번째 입금$");
+            System.out.print("동전 종류: ");
+            int type = sc.nextInt();
+            System.out.print("동전 갯수: ");
+            int number = sc.nextInt();
+
+            cc.addCoins(type, number);
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("===== 지갑 현황 =====");
+
+        for (int i : coin) {
+            System.out.println(i + "원 : " + cc.getCount(i) + "개");  // 동전 갯수 나열
+        }
+
+        System.out.println();
+        System.out.println("총액 : " + cc.getAmount() + "원");        // 총액
+    }
+
+
+    // Coin Case 클래스 선언
+    public static class CoinCase {
+
+        int[] type = new int[6];                        // 배열 선언 {1원, 5원, 10원, 50원, 100원, 500원}
+        int[] coin = {1, 5, 10, 50, 100, 500};
+
+        public void addCoins(int ct, int cn) {
+
+            for (int i = 0; i < type.length; i++) {     // 동전 분류 및 추가
+                if (ct == coin[i]) {
+                    type[i] += cn;
+                    return;
+                }
+            }
+            System.out.println("유효하지 않은 동전 종류");
+        }
+
+        public int getCount(int ct) {
+            for (int i = 0; i < type.length; i++) {     // 동전 갯수 리턴
+                if (ct == coin[i])
+                    return type[i];
+            }
+            System.out.println("유효하지 않은 동전 종류");
+            return 0;
+        }
+
+
+        public int getAmount() {
+
+            int total = 0;
+
+            for (int i = 0; i < type.length; i++) {     // 동전 총액
+                total += type[i] * coin[i];
+            }
+            return total;
+        }
+    }
+}
