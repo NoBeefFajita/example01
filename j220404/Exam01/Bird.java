02b83cc41ded9863752fc44d70cc301cc3ce6ff5
NoBeefFajita <tjdudwns96@gmail.com>
2022-04-05 06:43:47 +0900

    뻐꾹

--- j220404/Exam01/Bird.java
+++ j220404/Exam01/Bird.java
@@ -3,6 +3,6 @@ package example01.j220404.Exam01;
 public class Bird extends Animal{
     @Override
     public void sing() {
-        System.out.println("뻐꾹 뻐국");
+        System.out.println("뻐꾹 뻐꾹");
     }
 }
